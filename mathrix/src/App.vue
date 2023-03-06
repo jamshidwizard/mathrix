f0f3eb02a2da113a6138e7ab200c68a72a1f19e4
jamshid <jamshidtulaganov3101@gmail.com>
2023-03-06 13:40:10 +0500

    App header is added

--- mathrix/src/App.vue
+++ mathrix/src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import AppHeader from "./components/layouts/AppHeader.vue";
 </script>
 
 <template>
+  <app-header />
     <main>
         <router-view :key="$route.fullPath" />
     </main>
---
+++ mathrix/src/components/layouts/AppHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="header">
+      <div class="container d-flex align-items-center">
+          <icon-logo />
+          <div class="header__search d-flex align-items-center ms-auto me-3">
+            <icon-search />
+            <input type="text" placeholder="Find what interests you"/>
+          </div>
+          <div class="header__terminal me-3">
+            <select class="form-select" aria-label="Default select example">
+              <option selected>Terminal ES</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="header__premium me-3">
+            <button class="btn text-secondary">Get Premium</button>
+          </div>
+          <div class="header__actions">
+              <button class="header__actions--user btn me-3">
+                <icon-user />
+              </button>
+            <button class="header__actions--notification btn">
+              <icon-notification />
+            </button>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+import IconLogo from "../icons/IconLogo.vue";
+import IconSearch from "../icons/IconSearch.vue";
+import IconUser from "../icons/IconUser.vue";
+import IconNotification from "../icons/IconNotification.vue";
+export default {
+  name: "AppHeader",
+  components: { IconNotification, IconUser, IconSearch, IconLogo }
+};
+</script>
+
+<style scoped>
+
+</style>
